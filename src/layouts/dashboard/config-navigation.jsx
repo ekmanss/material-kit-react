2d9365b204b1a24d677779041c199114b98564a0
ekmanss <hfers@msn.com>
2024-06-26 13:28:55 +0800

    hide nav

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -7,36 +7,36 @@ const icon = (name) => (
 );
 
 const navConfig = [
-  {
-    title: 'dashboard',
-    path: '/',
-    icon: icon('ic_analytics'),
-  },
+  // {
+  //   title: 'dashboard',
+  //   path: '/',
+  //   icon: icon('ic_analytics'),
+  // },
   {
     title: 'user',
     path: '/user',
     icon: icon('ic_user'),
   },
-  {
-    title: 'product',
-    path: '/products',
-    icon: icon('ic_cart'),
-  },
-  {
-    title: 'blog',
-    path: '/blog',
-    icon: icon('ic_blog'),
-  },
-  {
-    title: 'login',
-    path: '/login',
-    icon: icon('ic_lock'),
-  },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: icon('ic_disabled'),
-  },
+  // {
+  //   title: 'product',
+  //   path: '/products',
+  //   icon: icon('ic_cart'),
+  // },
+  // {
+  //   title: 'blog',
+  //   path: '/blog',
+  //   icon: icon('ic_blog'),
+  // },
+  // {
+  //   title: 'login',
+  //   path: '/login',
+  //   icon: icon('ic_lock'),
+  // },
+  // {
+  //   title: 'Not found',
+  //   path: '/404',
+  //   icon: icon('ic_disabled'),
+  // },
 ];
 
 export default navConfig;
--- src/layouts/dashboard/nav.jsx
+++ src/layouts/dashboard/nav.jsx
@@ -112,13 +112,13 @@ export default function Nav({ openNav, onCloseNav }) {
     >
       <Logo sx={{ mt: 3, ml: 4 }} />
 
-      {renderAccount}
+      {/*{renderAccount}*/}
 
       {renderMenu}
 
       <Box sx={{ flexGrow: 1 }} />
 
-      {renderUpgrade}
+      {/*{renderUpgrade}*/}
     </Scrollbar>
   );
 
